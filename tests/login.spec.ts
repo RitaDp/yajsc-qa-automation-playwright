459139edfeeb67210ac2b19a9aeac2ffe8b3e231
Marharyta Rovna <marharyta.rovna@binary-studio.com>
2025-12-15 10:49:26 +0200

    simplify approach for credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-  const EMAIL = process.env.TEST_EMAIL!;
-  const PASSWORD = process.env.TEST_PASSWORD!;
-
 test.describe("Login", () => {
   test("Verify login with valid credentials navigates to account page", async ({ page }) => {
     await page.goto("/auth/login");
     await expect(page).toHaveURL(/.*\/auth\/login/);
 
-    await page.fill("#email", EMAIL);
-    await page.fill("#password", PASSWORD);
+    await page.getByPlaceholder('email').fill('customer@practicesoftwaretesting.com');
+    await page.getByPlaceholder('password').fill('welcome01');
+
     await page.locator('[data-test="login-submit"]').click();
 
     await expect(page.locator('[data-test="page-title"]')).toHaveText("My account");
