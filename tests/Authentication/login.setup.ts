16d1ef611950a5f506cc9c98811607882bd90298
Marharyta Rovna <marharyta.rovna@binary-studio.com>
2026-01-05 11:06:09 +0200

    login: add helpers, setAuthToken method

--- tests/Authentication/login.setup.ts
+++ tests/Authentication/login.setup.ts
@@ -1,28 +1,20 @@
-import { test as setup, expect } from '@playwright/test';
-import { HomePage } from '../../src/page-objects/home.page';
+import { test as setup } from '@playwright/test';
 import path from 'path';
+import { LoginPage } from '../../src/page-objects/login.page';
+import { getApiLoginToken, UserCredentials } from '../../src/utils/auth-helper';
 
 const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
 
-setup('Authenticate as valid user', async ({ request, page }) => {
-  const homePage = new HomePage(page);
+const credentials: UserCredentials = {
+  email: process.env.TEST_EMAIL!,
+  password: process.env.TEST_PASSWORD!,
+};
 
-  const response = await request.post('https://api.practicesoftwaretesting.com/users/login', {
-    data: {
-      email: process.env.TEST_EMAIL,
-      password: process.env.TEST_PASSWORD,
-    },
+setup('Authenticate as valid user', async ({ page, request }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.setAuthToken(async () => {
+    return getApiLoginToken(request, credentials);
   });
-
-  expect(response.ok()).toBeTruthy();
-  const responseBody = await response.json() as { access_token: string };;
-  const token = responseBody.access_token;
-
-  await homePage.navigateHomePage();
-
-  await page.evaluate((jwt) => {
-    localStorage.setItem('auth-token', jwt);
-  }, token);
-
+  
   await page.context().storageState({ path: authFile });
 });
