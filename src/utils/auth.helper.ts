263020719a24c354db6b024d49fe5c2d48f1b440
RitaDp <rmsdnepr@gmail.com>
2026-01-24 13:45:15 +0200

    add logs

--- src/utils/auth.helper.ts
+++ src/utils/auth.helper.ts
@@ -16,7 +16,15 @@ export async function performApiLogin (request: APIRequestContext, user: UserCre
 
 export async function getApiLoginToken (request: APIRequestContext, user: UserCredentials): Promise<string> {
   const response = await performApiLogin(request, user);
-  expect(response.ok()).toBeTruthy();
+  if (!response.ok()) {
+    const errorStatus = response.status();
+    const errorText = await response.text();
+    console.error(`API Login Failed! Status: ${errorStatus}`);
+    console.error(`Response: ${errorText}`);
+    console.error(`Sending Email: ${user.email ? 'Provided' : 'EMPTY'}`);
+    console.error(`Sending Password: ${user.password ? 'Provided' : 'EMPTY'}`);
+  }
+  expect(response.ok(), `Failed to login via API. Status: ${response.status()}`).toBeTruthy();
   const responseBody = await response.json() as { access_token: string };
   return responseBody.access_token;
 };
